ca08da71511b0096c47fd3453e37eee0bab17a21
Foundations-of-Frontend <azra.arnautovic@stu.ibu.edu.ba>
2024-05-05 23:21:29 +0200

    second week push

--- PROJECT_I_II/java/dynamic.js
+++ PROJECT_I_II/java/dynamic.js
@@ -19,3 +19,56 @@ function openModal() {
         }
     }
 }
+
+
+$(document).ready(function() {
+    // Adding custom method to check password strength
+    $.validator.addMethod("strongPassword", function(value, element) {
+      return this.optional(element) || zxcvbn(value).score >= 3;
+    }, "Password is too weak");
+  
+    // Initializing form validation
+    $("#contactForm").validate({
+      rules: {
+        name: "required",
+        email: {
+          required: true,
+          email: true
+        },
+        password: {
+          required: true,
+          strongPassword: true
+        },
+        message: "required"
+      },
+      messages: {
+        name: "Please enter your name",
+        email: {
+          required: "Please enter your email address",
+          email: "Please enter a valid email address"
+        },
+        password: {
+          required: "Please enter a password"
+        },
+        message: "Please enter your message"
+      },
+      errorElement: "span",
+      errorPlacement: function(error, element) {
+        error.addClass("invalid-feedback");
+        element.closest(".form-row").append(error);
+      },
+      highlight: function(element, errorClass, validClass) {
+        $(element).addClass("is-invalid").removeClass("is-valid");
+      },
+      unhighlight: function(element, errorClass, validClass) {
+        $(element).addClass("is-valid").removeClass("is-invalid");
+      }
+    });
+  
+    // Update password strength indicator
+    $("#password").on("input", function() {
+      var result = zxcvbn($(this).val());
+      $("#password-strength").html("Password strength: " + result.score + "/4");
+    });
+  });
+  
